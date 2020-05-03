17b07d2205e1dd7f8857f344a4c8980d22fad920
Ayorinde <ajibaye@gmail.com>
2020-05-03 21:56:13 +0100

    first trial

---
+++ index.js
@@ -0,0 +1,27 @@
+function add()
+{
+        let num1 = document.getElementById("first-input").value;
+        let num2 = document.getElementById("second-input").value;
+        document.getElementById("result").innerHTML = parseInt(num1) + parseInt(num2);
+}
+
+function subtract()
+{
+        let num1 = document.getElementById("first-input").value;
+        let num2 = document.getElementById("second-input").value;
+        document.getElementById("result").innerHTML = num1 - num2;
+}
+
+function multiply()
+{
+        let num1 = document.getElementById("first-input").value;
+        let num2 = document.getElementById("second-input").value;
+        document.getElementById("result").innerHTML = num1 * num2;
+}
+
+function divide() 
+{ 
+        let num1 = document.getElementById("first-input").value;
+        let num2 = document.getElementById("second-input").value;
+        document.getElementById("result").innerHTML = num1 / num2;
+}
