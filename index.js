cc620e02d7b3685904e16f400e05b0231ce7658f
Ayorinde <ajibaye@gmail.com>
2020-05-20 12:22:32 +0100

    input display

--- index.js
+++ index.js
@@ -3,14 +3,17 @@ function add()
 {
         let num1 = document.getElementById("first-input").value;
         let num2 = document.getElementById("second-input").value;
-        document.getElementById("result").innerHTML = parseInt(num1) + parseInt(num2);
+        if(num1 === '' || num2 === ''){
+                alert('input your values')
+        }
+        document.getElementById("result").value = parseInt(num1) + parseInt(num2);
 }
 
 function subtract()
 {
         let num1 = document.getElementById("first-input").value;
         let num2 = document.getElementById("second-input").value;
-        document.getElementById("result").innerHTML = num1 - num2;
+        document.getElementById("result").value = num1 - num2;
 }
 
 function multiply()
